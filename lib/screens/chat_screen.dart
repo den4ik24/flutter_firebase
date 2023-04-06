533646b2510138ae7d15f290749a7e0783af7906
Denis Yurchenko <den4ik24@gmail.com>
2023-04-06 22:36:07 +0300

    first commit

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text("This works!"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection("chats/1MTLBSqXY0NClGdJaHBJ/messages")
+              .snapshots()
+              .listen((data) {
+            for (var document in data.docs) {
+              print(document["text"]);
+            }
+          });
+        },
+      ),
+    );
+  }
+}
