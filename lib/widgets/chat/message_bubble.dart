409660c47319996e93f0a45f90d47659bc586a7a
Denis Yurchenko <den4ik24@gmail.com>
2023-04-27 20:13:03 +0300

    userNames in bubbles

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MessageBubble extends StatelessWidget {
-  const MessageBubble(this.message, this.isMe, {super.key});
+  const MessageBubble(this.message, this.userName, this.isMe, {super.key});
 
   final String message;
+  final String userName;
   final bool isMe;
 
   @override
@@ -36,14 +38,30 @@ class MessageBubble extends StatelessWidget {
             vertical: 4,
             horizontal: 8,
           ),
-          child: Text(
-            message,
-            style: TextStyle(
-              //color: Theme.of(context).accentTextTheme.titleMedium!.color,
-              color: isMe
-                  ? Colors.black
-                  : Theme.of(context).colorScheme.onSecondary,
-            ),
+          child: Column(
+            crossAxisAlignment:
+                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                userName,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe
+                      ? Colors.black
+                      : Theme.of(context).colorScheme.onSecondary,
+                ),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  //color: Theme.of(context).accentTextTheme.titleMedium!.color,
+                  color: isMe
+                      ? Colors.black
+                      : Theme.of(context).colorScheme.onSecondary,
+                ),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
